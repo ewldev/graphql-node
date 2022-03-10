2d036f9f23cdfb928c0e108d1b87154f58327089
eleung32 <52718704+eleung32@users.noreply.github.com>
2022-03-09 22:47:19 -0500

    adding graphqlHTTP and rootValue

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
 
 const schema = buildSchema(`
-    type Query
+    type Query {
+        description: String
+        price: Float
+    }
 `);
 
+const root = {
+    description: 'Blue T-Shirt',
+    price: 30.12,
+};
+
 const app = express();
+
+app.use(graphqlHTTP({
+    schema: schema,
+    rootValue:
+}))
 
 app.listen(3000, () => {
     console.log('Running GraphQL server...');
