a9a94ae4a835f86b086078fbc9f71e31e415358b
eleung32 <52718704+eleung32@users.noreply.github.com>
2022-03-11 14:50:26 -0500

    movded const root data to orders and products.model.js

---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        date: '2021-3-20',
+        subtotal: 51.00,
+        items: [ 
+          {
+            product: {
+              id: 'bluetshirt',
+              description: 'Old Blue T-Shirt',
+              price: 25.50,
+            },
+            quantity: 2,
+          }
+        ]
+      }
+];
---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+{
+    id: 'bluetshirt',
+    description: 'Blue T-Shirt',
+    price: 30.95,
+  },
+  {
+    id: 'greypant',
+    description: 'Grey Pants',
+    price: 79.85,
+  }
+]; 
